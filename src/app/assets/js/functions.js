45b1d2aecf2e8d9d546454c6fe5e00dcf94cdacb
Laion12 <pandaerox@gmail.com>
2021-04-16 06:06:37 -0500

    service model

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -1,7 +1,7 @@
 var head=document.getElementById('header');
 var contUs=document.getElementById('cont-serv');
 
-contUs.addEventListener("mouseover",function(){
+contUs.addEventListener("wheel",function(){
     contUs.classList.remove("cont-serv");
     contUs.classList.add("cont-serv1");
 })
