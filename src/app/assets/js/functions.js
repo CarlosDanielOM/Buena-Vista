baf4aa72a86c62138216e08afa1eb4160710dd9e
Laion12 <pandaerox@gmail.com>
2021-04-19 04:07:28 -0500

    animation service1

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -7,12 +7,18 @@ var contUs=document.getElementById('cont-serv');
 })*/
 var link1=document.getElementById('link-serv1');
 var link2=document.getElementById('link-serv2');
+var serv1=document.getElementById('serv1');
+var serv2=document.getElementById('serv2');
 
 link1.addEventListener("click",function(){
     link1.classList.add("link-active");
     link2.classList.remove("link-active");
+    serv2.style.width ="0%";
+    serv1.style.width ="100%";
 })
 link2.addEventListener("click",function(){
     link2.classList.add("link-active");
     link1.classList.remove("link-active");
+    serv1.style.width ="0%";
+    serv2.style.width ="100%";
 })
