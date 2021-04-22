06be0dbd2da0e00a8c669f5821cd0c1c3264143a
Laion12 <pandaerox@gmail.com>
2021-04-22 04:26:54 -0500

    create gallery

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -13,27 +13,31 @@ var serv2=document.getElementById('serv2');
 link1.addEventListener("click",function(){
     link1.classList.add("link-active");
     link2.classList.remove("link-active");
-    serv2.style.width ="0%";
+    /*serv2.style.display ="none";
     serv1.style.width ="100%";
+    serv1.style.height ="100%";
+    serv1.style.display ="flex";*/
+    serv2.style.marginLeft ="-100%";
 })
 link2.addEventListener("click",function(){
     link2.classList.add("link-active");
     link1.classList.remove("link-active");
-    serv1.style.width ="0%";
+    /*serv1.style.display ="none";
     serv2.style.width ="100%";
+    serv2.style.height ="100%"
+    serv2.style.display ="flex";*/
+    serv2.style.marginLeft ="0%";
 })
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
 var img_b=document.getElementById('img-abt');
 
 main_scroll.addEventListener("scroll",function(){
-    var h=window.innerHeight/1.2;
-    var h1=window.innerHeight;
+    var h=window.innerHeight;
     var d=text_b.getBoundingClientRect().top;
     console.log(h);
-    console.log(h1);
     console.log(d);
-    if(d<=640 & d>=-300){
+    if(d<=(h-100) & d>=(h-1100)){
         text_b.classList.add("transitionX");
         img_b.classList.add("transitionUp");
     }else{
