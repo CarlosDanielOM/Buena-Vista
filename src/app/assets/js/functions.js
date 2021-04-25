b2b8ef2932388f9558d1333a7d2e482041a5532a
Laion12 <pandaerox@gmail.com>
2021-04-25 03:59:52 -0500

    update cont img

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -1,10 +1,5 @@
 var head=document.getElementById('header');
 var contUs=document.getElementById('cont-serv');
-
-/*contUs.addEventListener("wheel",function(){
-    contUs.classList.remove("cont-serv");
-    contUs.classList.add("cont-serv1");
-})*/
 var link1=document.getElementById('link-serv1');
 var link2=document.getElementById('link-serv2');
 var serv1=document.getElementById('serv1');
@@ -13,20 +8,14 @@ var serv2=document.getElementById('serv2');
 link1.addEventListener("click",function(){
     link1.classList.add("link-active");
     link2.classList.remove("link-active");
-    /*serv2.style.display ="none";
-    serv1.style.width ="100%";
-    serv1.style.height ="100%";
-    serv1.style.display ="flex";*/
-    serv2.style.marginLeft ="-100%";
+    serv2.style.display ="none";
+    serv1.style.display ="flex";
 })
 link2.addEventListener("click",function(){
     link2.classList.add("link-active");
     link1.classList.remove("link-active");
-    /*serv1.style.display ="none";
-    serv2.style.width ="100%";
-    serv2.style.height ="100%"
-    serv2.style.display ="flex";*/
-    serv2.style.marginLeft ="0%";
+    serv1.style.display ="none";
+    serv2.style.display ="flex";
 })
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
