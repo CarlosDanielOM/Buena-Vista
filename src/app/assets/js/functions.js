2f9c13e4730920df73e5a72d0ce6b0b820d9f601
Laion12 <pandaerox@gmail.com>
2021-04-28 04:41:12 -0500

    debug animation service

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -4,22 +4,25 @@ var link1=document.getElementById('link-serv1');
 var link2=document.getElementById('link-serv2');
 var serv1=document.getElementById('serv1');
 var serv2=document.getElementById('serv2');
-
 link1.addEventListener("click",function(){
     link1.classList.add("link-active");
     link2.classList.remove("link-active");
-    serv2.style.opacity ="0";
+    /*serv2.style.opacity ="0";
     serv2.style.display ="none";
     serv1.style.opacity ="1";
-    serv1.style.display ="flex";
+    serv1.style.display ="flex";*/
+    serv1.classList.remove("output");
+    serv2.classList.remove("input");
 })
 link2.addEventListener("click",function(){
     link2.classList.add("link-active");
     link1.classList.remove("link-active");
-    serv1.style.opacity ="0";
+    /*serv1.style.opacity ="0";
     serv1.style.display ="none";
     serv2.style.opacity ="1";
-    serv2.style.display ="flex";
+    serv2.style.display ="flex";*/
+    serv1.classList.add("output");
+    serv2.classList.add("input");
 })
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
