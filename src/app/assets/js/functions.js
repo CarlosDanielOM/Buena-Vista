46f79c9d7a9baf9e3f12863c6a4983e0713f25c4
Laion12 <pandaerox@gmail.com>
2021-05-03 05:40:56 -0500

    add contact animation

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -7,38 +7,37 @@ var serv2=document.getElementById('serv2');
 link1.addEventListener("click",function(){
     link1.classList.add("link-active");
     link2.classList.remove("link-active");
-    /*serv2.style.opacity ="0";
-    serv2.style.display ="none";
-    serv1.style.opacity ="1";
-    serv1.style.display ="flex";*/
     serv1.classList.remove("output");
     serv2.classList.remove("input");
 })
 link2.addEventListener("click",function(){
     link2.classList.add("link-active");
     link1.classList.remove("link-active");
-    /*serv1.style.opacity ="0";
-    serv1.style.display ="none";
-    serv2.style.opacity ="1";
-    serv2.style.display ="flex";*/
     serv1.classList.add("output");
     serv2.classList.add("input");
 })
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
 var img_b=document.getElementById('img-abt');
-
+var contact_b=document.getElementById('data-contact');
 main_scroll.addEventListener("scroll",function(){
     var h=window.innerHeight;
     var d=text_b.getBoundingClientRect().top;
+    var x=contact_b.getBoundingClientRect().top;
     console.log(h);
     console.log(d);
+    console.log(x);
     if(d<=(h-100) & d>=(h-1100)){
         text_b.classList.add("transitionX");
         img_b.classList.add("transitionUp");
     }else{
         text_b.classList.remove("transitionX");
         img_b.classList.remove("transitionUp");
+    }
+    if(x>=(h-20)){
+        contact_b.classList.add("transitionDown");
+    }else{
+        contact_b.classList.remove("transitionDown");
     }
 })
 
