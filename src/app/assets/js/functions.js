e57a1da5057615ec4df5814e385eb25f6dbfb839
Laion12 <pandaerox@gmail.com>
2021-05-06 04:09:31 -0500

    fix services

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -1,6 +1,6 @@
 var head=document.getElementById('header');
 var contUs=document.getElementById('cont-serv');
-var link1=document.getElementById('link-serv1');
+/*var link1=document.getElementById('link-serv1');
 var link2=document.getElementById('link-serv2');
 var serv1=document.getElementById('serv1');
 var serv2=document.getElementById('serv2');
@@ -15,7 +15,7 @@ link2.addEventListener("click",function(){
     link1.classList.remove("link-active");
     serv1.classList.add("output");
     serv2.classList.add("input");
-})
+})*/
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
 var img_b=document.getElementById('img-abt');
