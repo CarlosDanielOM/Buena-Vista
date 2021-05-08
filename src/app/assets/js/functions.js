3d9f457a3fc8d61937a5a0e06d74593d66448e1e
Laion12 <pandaerox@gmail.com>
2021-05-08 05:04:08 -0500

    aditional effects

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -1,21 +1,5 @@
 var head=document.getElementById('header');
 var contUs=document.getElementById('cont-serv');
-/*var link1=document.getElementById('link-serv1');
-var link2=document.getElementById('link-serv2');
-var serv1=document.getElementById('serv1');
-var serv2=document.getElementById('serv2');
-link1.addEventListener("click",function(){
-    link1.classList.add("link-active");
-    link2.classList.remove("link-active");
-    serv1.classList.remove("output");
-    serv2.classList.remove("input");
-})
-link2.addEventListener("click",function(){
-    link2.classList.add("link-active");
-    link1.classList.remove("link-active");
-    serv1.classList.add("output");
-    serv2.classList.add("input");
-})*/
 var main_scroll=document.getElementById('main');
 var text_b=document.getElementById('cont-text');
 var img_b=document.getElementById('img-abt');
