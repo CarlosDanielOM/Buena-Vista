0fd58e047fadbb655dfbd5be9c7e996812ac8d00
Carlos Daniel Ordoñez Matus <43821176+CarlosDanielOM@users.noreply.github.com>
2021-06-07 20:06:21 -0700

    Frontend (#1)
    * New front-end commit created.
    
    * Added empty files for github.
    
    * update struct
    
    * create main desing
    
    * update slider
    
    * slider optimization
    
    * service section
    
    * animations
    
    * about content design
    
    * service model
    
    * update services content
    
    * animation service1
    
    * update animations
    
    * update about-animations
    
    * create gallery
    
    * update cont img
    
    * bugs fixed
    
    * debug animation service
    
    * img services
    
    * design card service
    
    * fixed small designs
    
    * add contact animation
    
    * fix services
    
    * aditional effects
    
    * responsive big
    
    * add responsive medium
    
    * fixed design medium
    
    * fixed responsive medium
    
    * aditional fix
    
    * fixed medium design
    
    * responsive small
    
    * responsive movile small fix
    
    * fixed small responsive
    
    * contact renovated
    
    * .
    
    * Updating assets paths
    
    * contact serv  add
    
    * contact fixed
    
    * animatios email us
    
    * responsive and animatios
    
    * small fix email
    
    Co-authored-by: Laion12 <pandaerox@gmail.com>

---
+++ src/app/assets/js/functions.js
@@ -0,0 +1,22 @@
+var head=document.getElementById('header');
+var contUs=document.getElementById('cont-serv');
+var main_scroll=document.getElementById('main');
+var text_b=document.getElementById('cont-text');
+var img_b=document.getElementById('img-abt');
+var contact_b=document.getElementById('data-contact');
+main_scroll.addEventListener("scroll",function(){
+    var h=window.innerHeight;
+    var d=text_b.getBoundingClientRect().top;
+    var x=contact_b.getBoundingClientRect().top;
+    /*console.log(h);
+    console.log(d);
+    console.log(x);*/
+    if(d<=(h-100) & d>=(h-1100)){
+        text_b.classList.add("transitionX");
+        img_b.classList.add("transitionUp");
+    }else{
+        text_b.classList.remove("transitionX");
+        img_b.classList.remove("transitionUp");
+    }
+})
+
