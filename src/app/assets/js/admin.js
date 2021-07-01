5273c79647a0ef1b03ca2ff7d2ca175b27e31487
Laion12 <pandaerox@gmail.com>
2021-07-01 05:14:14 -0500

    titles efects

--- src/app/assets/js/admin.js
+++ src/app/assets/js/admin.js
@@ -2,9 +2,10 @@ var sec1=document.getElementById('sec1');
 var sec2=document.getElementById('sec2');
 var sec3=document.getElementById('sec3');
 var usr=document.getElementById('user');
+var list=document.getElementsByClassName('list');
 var cont_sec=0;
 var mod=document.getElementsByClassName('content-mod');
-console.log(mod);
+console.log(list);
 sec1.addEventListener("click",function(){
     cont_sec=1;
     change_sec(cont_sec);
@@ -19,6 +20,8 @@ sec3.addEventListener("click",function(){
 });
 function change_sec(x){
     var c=x;
+    var c2=x-1;
+    console.log(c2);
     for(i=0;i<=3;i++){
         if(i==c){
             mod[i].classList.remove("shadow");
@@ -26,6 +29,13 @@ function change_sec(x){
         }else{
             mod[i].classList.remove("show");
             mod[i].classList.add("shadow");
+        }
+    }
+    for(i=0;i<=2;i++){
+        if(i==c2){
+            list[i].classList.add("text-select");
+        }else{
+            list[i].classList.remove("text-select");
         }
     }
 }
